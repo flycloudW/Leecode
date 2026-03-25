339a60acfb414f85e53195208d8a713c6607ca8c
Hermes <2808179434@qq.com>
2026-03-25 15:17:56 +0800

    Implement cycle detection in linked list

---
+++ hot100/链表/easy_141_环形链表/Soultion.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ // 快慢指针一定会在环形链表里面相遇
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
